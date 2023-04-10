c5ba89841e55c57601a3c38a0c25ce8463761af6
Alinzzz230 <130183063+Alinzzz230@users.noreply.github.com>
2023-04-09 23:38:31 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        animal = request.form["animal"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(animal),
+            temperature=0.7,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(animal):
+    return """Suggest four alternate names for the word to describe it..
+
+Animal: Cat
+Names: Kitty, Feline, Pussycat, Tabby
+Animal: Dog
+Names: Puppy, Doggy, Canine, Mutt
+Animal: {}
+Names:""""".format(
+        animal.capitalize()
+    )
